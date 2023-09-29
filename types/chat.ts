dbf193e021e4732935867764385cc1746c60353b
Vegas <vegaslancer1025@gmail.com>
2023-09-29 22:20:54 +0800

    [Update] Implement DatePicker and User Feedback User Form

--- types/chat.ts
+++ types/chat.ts
@@ -3,5 +3,7 @@ import { Document } from 'langchain/document';
 export type Message = {
   type: 'apiMessage' | 'userMessage';
   message: string;
+  data: [];
+  format: string;
   isStreaming?: boolean;
 };
